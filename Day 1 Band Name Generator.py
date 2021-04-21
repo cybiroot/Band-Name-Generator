74ab2216c2704b97ddf92ef53b646721a703d3dc
knownascybi <cybi.mcx@gmail.com>
2021-04-21 13:08:51 +0300

    v .1

---
+++ Day 1 Band Name Generator.py
@@ -0,0 +1,8 @@
+# Day 1 Project : Band Name generator
+
+print ("This is a band name generator!")
+
+city = input("What's the name of the city you grew up in?\n")
+pets_name = input("What's your pet's name?\n")
+
+print (city + " " + pets_name)
